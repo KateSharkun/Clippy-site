3551d829c6a1722764952f80c92d3472d5a71337
KateSharkun <90849014+KateSharkun@users.noreply.github.com>
2023-06-23 20:56:39 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-#https://unpkg.com/clippyts@1.0.4/dist/index.js
 
 @app.route("/",methods=["GET"])
 @app.route("/home",methods=["GET"])
